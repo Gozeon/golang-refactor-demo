6e15f38971f728b35185f40b2fe76eca8d16ab42
goze <goze.qiu@gmail.com>
2017-07-25 22:54:49 +0800

    feat: add echo4

--- echo4/main.go
+++ echo4/main.go
@@ -1,1 +1,23 @@
-package echo4
+// Echo4 prints its command-line arguments.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var n = flag.Bool("n", false, "omit trailing newline")
+var sep = flag.String("s", " ", "separator")
+
+func main() {
+	flag.Parse()
+	fmt.Print(strings.Join(flag.Args(), *sep))
+	if !*n {
+		fmt.Println()
+	}
+}
+// bash ./main a bc def
+// bash ./main -s / a bc def
+// bash ./main -n a bc def
+// bash ./main -help
