c84a472d3d1f91f5deca2a300bc6f9cb1a91db19
goze <goze.qiu@gmail.com>
2017-08-24 23:32:28 +0800

    demo 2

---
+++ scopes/scopes.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "reflect"
+
+func f() {}
+
+var g = "g"
+
+// func main() {
+// 	f := "f"
+// 	fmt.Println(f) // 局部变量覆盖了包级函数
+// 	fmt.Println(g) // 包级变量
+// 	// fmt.Println(h) // error
+// }
+
+func main() {
+	x := "hello!"
+	for i := 0; i < len(x); i++ {
+		x := x[i]
+		if x != '!' {
+			fmt.Println(reflect.TypeOf(x))
+			x := x + 'A' - 'a'
+			fmt.Printf("%c", x) // HELLO
+		}
+	}
+}
