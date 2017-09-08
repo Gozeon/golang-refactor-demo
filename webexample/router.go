8c1a3b3a0304aa6b82d6d42fba8d0c6c5bf882c4
goze <goze.qiu@gmail.com>
2017-09-08 15:04:03 +0800

    feat(web-example): router

--- webexample/router.go
+++ webexample/router.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/books/{title}/page/{page}", func(writer http.ResponseWriter, request *http.Request) {
+		vars := mux.Vars(request)
+		title := vars["title"]
+		page := vars["page"]
+
+		fmt.Fprintf(writer, "you've requested the book: %s on page %s\n", title, page)
+	})
+
+	http.ListenAndServe("localhost:8080", r)
+}
